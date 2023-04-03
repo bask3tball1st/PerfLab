f963431940d3c8f997248a9200017d37a38e627d
Savinov Petr <petrsavinov98@gmail.com>
2023-04-03 18:12:23 +0300

    Добавил проверку на пустой файл

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task4
 {
@@ -14,21 +10,30 @@ namespace Task4
             string path = Environment.CurrentDirectory + "/input.txt";
             int sum = 0;
             string[] nums = File.ReadAllLines(path);
-            int[] numbers = new int[nums.Length];
-            for (int i = 0; i < nums.Length; i++)
+            if (nums.Length != 0)
             {
-                numbers[i] = Convert.ToInt32(nums[i]);
-                sum += numbers[i];
+                int[] numbers = new int[nums.Length];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    numbers[i] = Convert.ToInt32(nums[i]);
+                    sum += numbers[i];
+                }
+                int avg = sum / numbers.Length;
+                int count = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    count += Math.Abs(numbers[i] - avg);
+                }
+                Console.WriteLine("Answer: " + count);
+                Console.WriteLine("Press any key to close...");
+                Console.ReadLine();
             }
-            int avg = sum / numbers.Length;
-            int count = 0;
-            for (int i = 0; i < numbers.Length; i++)
+            else
             {
-                count += Math.Abs(numbers[i] - avg);
+                Console.WriteLine("File is empty! Fill in the input.txt file.");
+                Console.WriteLine("Press any key to close...");
+                Console.ReadLine();
             }
-            Console.WriteLine("Answer: " + count);
-            Console.WriteLine("Press any key to close...");
-            Console.ReadLine();
         }
     }
 }
